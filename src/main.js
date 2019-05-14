5c110250ed61eb665fbc7070cf7304cbc448ff03
rositadiazgalaz <rositadiazgalaz@gmail.com>
2019-05-14 15:18:52 -0400

    funcion mostrar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-/* Manejo del DOM */
+/* Manejo del DOM */        
+ const pokedata = POKEMON.pokemon;
+
+window.onload=()=>{
+    for(let i=0; i < pokedata.length; i++){
+        document.getElementById("root").innerHTML +=
+        `
+        <img src="${pokedata[i].img}" alt="hola">
+        <p>${pokedata[i].name}</p>
+        `
+    }  
+}
